1db30ce159c573e620a7c3e5599b74d4e507da35
stamplevskiyd <stamplevskiyd@gmail.com>
2025-06-25 16:18:09 +0300

    Remove idea (again)

--- owl_core/api/router.py
+++ owl_core/api/router.py
@@ -4,7 +4,7 @@ from owl_core.api.users.users_api import users_router
 from owl_core.api.titles.titles_api import titles_router
 from owl_core.api.tags.tags_api import tags_router
 
-api_router = APIRouter(prefix="/api/v1", tags=["v1"])
+api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(reviews_router)
 api_router.include_router(users_router)
